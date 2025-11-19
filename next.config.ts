2e1c5eb2d5f94f75c9f5f16b06dccc51036b5d51
Jeremiah Agenyi <JerryAgenyi@gmail.com>
2025-11-19 10:40:33 +0000

    use these to randomise profile pictures. https://mockmind-api.uifaces.co

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'mockmind-api.uifaces.co',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
