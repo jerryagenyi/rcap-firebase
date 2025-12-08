b49676579deb8a48888435ffca54946fd9cf2160
Jeremiah Agenyi <JerryAgenyi@gmail.com>
2025-12-08 15:04:46 +0000

    thanks for the button variant fix. For the rest, I do not want to work o

--- src/app/dashboard/activities/components/activity-form.tsx
+++ src/app/dashboard/activities/components/activity-form.tsx
@@ -23,6 +23,7 @@ import type { Activity, SemioticAssessment } from '@/lib/types';
 import SemioticAssessmentDisplay from './semiotic-assessment-display';
 import { Alert, AlertTitle, AlertDescription } from '@/components/ui/alert';
 import { Checkbox } from '@/components/ui/checkbox';
+import { states, statesAndLgas } from '@/lib/geodata';
 
 const formSchema = z.object({
   title: z.string().min(5, 'Title must be at least 5 characters.'),
@@ -59,14 +60,6 @@ type ActivityFormProps = {
 };
 
 // Mock data
-const statesAndLgas: Record<string, string[]> = {
-    "Abuja FCT": ["Abaji", "Bwari", "Gwagwalada", "Kuje", "Kwali", "Municipal Area Council"],
-    "Lagos": ["Agege", "Alimosho", "Ifako-Ijaiye", "Ikeja", "Kosofe", "Mushin", "Oshodi-Isolo", "Shomolu", "Apapa", "Eti-Osa", "Lagos Island", "Lagos Mainland", "Surulere", "Ojo", "Ajeromi-Ifelodun", "Amuwo-Odofin", "Badagry", "Ikorodu", "Ibeju-Lekki", "Epe"],
-    "Kano": ["Dala", "Fagge", "Gwale", "Kano Municipal", "Tarauni", "Nassarawa", "Kumbotso", "Ungogo", "Dawakin Tofa", "Tofa", "Rimin Gado", "Bagwai", "Gezawa", "Gabasawa", "Minjibir", "Dambatta", "Makoda", "Kunchi", "Bichi", "Tsanyawa", "Shanono", "Gwarzo", "Karaye", "Rogo", "Kabo", "Bunkure", "Kibiya", "Rano", "Tudun Wada", "Doguwa", "Madobi", "Kura", "Garun Mallam", "Bebeji", "Kiru", "Sumaila", "Garko", "Takai", "Albasu", "Gaya", "Wudil", "Warawa", "Ajingi"],
-    "Rivers": ["Port Harcourt", "Obio-Akpor", "Okrika", "Ogu/Bolo", "Eleme", "Tai", "Gokana", "Khana", "Oyigbo", "Opobo/Nkoro", "Andoni", "Bonny", "Degema", "Asari-Toru", "Akuku-Toru", "Abua/Odual", "Ahoada West", "Ahoada East", "Ogba/Egbema/Ndoni", "Emuoha", "Ikwerre", "Etche", "Omuma"],
-    "Borno": ["Maiduguri", "Jere"],
-};
-const states = Object.keys(statesAndLgas);
 const activityTypes = [...new Set(mockActivities.map(a => a.type))];
 const regions = ['Nigeria', 'UK', 'Germany'];
 const languages = ['English', 'Hausa', 'Yoruba', 'Igbo', 'Pidgin'];
---
+++ src/lib/geodata.ts
@@ -0,0 +1,10 @@
+
+export const statesAndLgas: Record<string, string[]> = {
+    "Abuja FCT": ["Abaji", "Bwari", "Gwagwalada", "Kuje", "Kwali", "Municipal Area Council"],
+    "Lagos": ["Agege", "Alimosho", "Ifako-Ijaiye", "Ikeja", "Kosofe", "Mushin", "Oshodi-Isolo", "Shomolu", "Apapa", "Eti-Osa", "Lagos Island", "Lagos Mainland", "Surulere", "Ojo", "Ajeromi-Ifelodun", "Amuwo-Odofin", "Badagry", "Ikorodu", "Ibeju-Lekki", "Epe"],
+    "Kano": ["Dala", "Fagge", "Gwale", "Kano Municipal", "Tarauni", "Nassarawa", "Kumbotso", "Ungogo", "Dawakin Tofa", "Tofa", "Rimin Gado", "Bagwai", "Gezawa", "Gabasawa", "Minjibir", "Dambatta", "Makoda", "Kunchi", "Bichi", "Tsanyawa", "Shanono", "Gwarzo", "Karaye", "Rogo", "Kabo", "Bunkure", "Kibiya", "Rano", "Tudun Wada", "Doguwa", "Madobi", "Kura", "Garun Mallam", "Bebeji", "Kiru", "Sumaila", "Garko", "Takai", "Albasu", "Gaya", "Wudil", "Warawa", "Ajingi"],
+    "Rivers": ["Port Harcourt", "Obio-Akpor", "Okrika", "Ogu/Bolo", "Eleme", "Tai", "Gokana", "Khana", "Oyigbo", "Opobo/Nkoro", "Andoni", "Bonny", "Degema", "Asari-Toru", "Akuku-Toru", "Abua/Odual", "Ahoada West", "Ahoada East", "Ogba/Egbema/Ndoni", "Emuoha", "Ikwerre", "Etche", "Omuma"],
+    "Borno": ["Maiduguri", "Jere"],
+};
+
+export const states = Object.keys(statesAndLgas);
